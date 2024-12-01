21a6d06320cfdff945eb750711c1e623ad1f4ebc
Ted Byfield <tbfld@users.noreply.github.com>
2024-12-01 09:56:26 -0500

    Quartz sync: Dec 1, 2024, 9:56 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -7,6 +7,7 @@ const config: QuartzConfig = {
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
+    enableColorScheme: false,  // Added this line
     analytics: {
       provider: "plausible",
     },
